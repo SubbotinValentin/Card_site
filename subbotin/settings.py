fea4d473ec979b10406ec239e982d49d37502bf8
SubbotinValentin <114022271+SubbotinValentin@users.noreply.github.com>
2022-12-22 04:16:38 +0500

    Update settings.py

--- subbotin/settings.py
+++ subbotin/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-trkzi9!nv_6gp(in&rw@z(=!+!@=!4*usrzu(4e$)b@-8%hk'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
